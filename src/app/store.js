251061d6ca7f922649e05eb82e2e94b2d162acde
Michel Reyes <57273411+m-reyes-h@users.noreply.github.com>
2021-02-26 11:02:04 -0500

    added server info logic

--- src/app/store.js
+++ src/app/store.js
@@ -2,6 +2,9 @@ import { configureStore } from "@reduxjs/toolkit";
 import postReducer from "../features/posts/postSlice";
 import userReducer from "../features/users/usersSlice";
 
+// The most common reason to use middleware is to allow different kinds of async logic
+// to interact with the store
+
 export default configureStore({
   reducer: {
     posts: postReducer,
