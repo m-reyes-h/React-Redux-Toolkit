d2e8266dc45eba2188e77a827152cc6ed6f26447
Michel Reyes <57273411+m-reyes-h@users.noreply.github.com>
2021-02-25 19:15:20 -0500

    last updated

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import { Navbar } from "./app/Navbar";
 
 import { PostsList } from "./features/posts/PostsList";
 import { AddPostForm } from "./features/posts/AddPostForm";
+import { SinglePostPage } from "./features/posts/SinglePostPage";
+import { EditPostForm } from "./features/posts/EditPostForm";
 
 function App() {
   return (
@@ -27,6 +29,12 @@ function App() {
                 <PostsList />
               </React.Fragment>
             )}
+          />
+          <Route exact path="/posts/:postId" component={SinglePostPage} />
+          <Route
+            exact
+            path="/posts/editPost/:postID"
+            component={EditPostForm}
           />
           <Redirect to="/" />
         </Switch>
--- src/features/posts/EditPostForm.jsx
+++ src/features/posts/EditPostForm.jsx
@@ -1,36 +1,42 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 
-import { postAdded } from "./postSlice";
+import { postUpdated } from "./postSlice";
 
-export const AddPostForm = () => {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
+export const EditPostForm = ({ match }) => {
+  const { postID } = match.params;
+
+  const post = useSelector((state) =>
+    state.posts.find((post) => post.id === postID)
+  );
+
+  const [title, setTitle] = useState(post.title);
+  const [content, setContent] = useState(post.content);
 
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const onTitleChanged = (e) => setTitle(e.target.value);
   const onContentChanged = (e) => setContent(e.target.value);
 
   const onSavePostClicked = () => {
     if (title && content) {
-      dispatch(postAdded(title, content));
-
-      // reset form values
-      setTitle("");
-      setContent("");
+      dispatch(postUpdated({ id: postID, title, content }));
+      history.push(`/posts/${postID}`);
     }
   };
 
   return (
     <section>
-      <h2>Add a New Post</h2>
+      <h2>Edit Post</h2>
       <form>
         <label htmlFor="postTitle">Post Title:</label>
         <input
           type="text"
           id="postTitle"
           name="postTitle"
+          placeholder="What's on your mind?"
           value={title}
           onChange={onTitleChanged}
         />
@@ -41,10 +47,10 @@ export const AddPostForm = () => {
           value={content}
           onChange={onContentChanged}
         />
-        <button type="button" onClick={onSavePostClicked}>
-          Save Post
-        </button>
       </form>
+      <button type="button" onClick={onSavePostClicked}>
+        Save Post
+      </button>
     </section>
   );
 };
